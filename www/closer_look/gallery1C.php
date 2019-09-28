f113fb3da43981022e8c48ef0454d087b30cf270
cander9 <49759197+cander9@users.noreply.github.com>
2019-09-28 08:00:20 -0400

    Create gallery1C.php

---
+++ www/closer_look/gallery1C.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang = "en">
+<head>
+     
+<!-- Author:  Caitlin Anderson
+     Purpose: CS321 Assignment 2 --> 
+
+  <link rel="stylesheet" href="/css/closer_style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
+</head>
+  <nav class = "header">         
+    <a><h2>Name</h2></a>
+    <ul>
+      <li class = crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
+      <li class = crumb><a href = "gallery 2">Gallery 2</a></li>
+      <li class = crumb><a href = "shop">Shop</a></li>
+      <li class = crumb><a href = "bio">Bio</a></li>
+    </ul>
+<!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
+  </nav>   <!-- I want the nav as a seperate column to the left -->
+  <header> <h1>Gallery 1</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </header>   <!-- will be a description of the gallery in final -->
+  <section>
+    <img   src="http://anderscr.cs321.simmons.edu/images/IMG2.JPG" width = 600/>
+  </section>   <!-- will be a picture of the art in final, I want it centered and big-->
+  <footer>
+    <a>copyright information</a>
+    <a href = "contact" class = "contact">Contact</a>
+  </footer>
+</html><!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
