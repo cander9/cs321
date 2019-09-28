d3967831827e6984dd4ff4f4dccb4609901a6aae
cander9 <49759197+cander9@users.noreply.github.com>
2019-09-28 07:59:23 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -19,7 +19,7 @@
     </ul>
 <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
   </nav>   <!-- I want the nav as a seperate column to the left -->
-  <header> <h1>Gallery Title</h1>
+  <header> <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </header>   <!-- will be a description of the gallery in final -->
  <ul>
