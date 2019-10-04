ebc36568e974c9b6ae469a4ab33befb2ff70ccf0
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-04 11:15:56 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -39,7 +39,7 @@
   </ul><!-- However Many I need --><!--Code Source:  https://www.w3schools.com/howto/howto_css_button_on_image.asp    eventually I want to be able to click on the img rather than have a separate button-->
   <footer>
     <a>copyright information</a>
-    <a href = "contact" class = "contact">Contact Me</a>
+    <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class = "contact">Contact Me</a>
   </footer>
 <!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
   </html>
