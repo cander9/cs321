9f2487a88febda13e2e4625921434a775fa612c4
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-04 10:49:56 -0400

    Update myscripts.js

--- www/javascript/myscripts.js
+++ www/javascript/myscripts.js
@@ -1,29 +1,5 @@
-  var test = document.getElementById("closerLook1");
-  
-  test.addEventListener("mouseover", function( event ) {   
-    event.target.style.opacity = 0.5;
-
-    setTimeout(function() {
-      event.target.style.opacity = 0;
-    }, 750);
-  }, false);
-
-  var test = document.getElementById("closerLook2");
-  
-  test.addEventListener("mouseover", function( event ) {   
-    event.target.style.opacity = 0.5;
-
-    setTimeout(function() {
-      event.target.style.opacity = 0;
-    }, 1000);
-  }, false);
-
-  var test = document.getElementById("closerLook3");
-  
-  test.addEventListener("mouseover", function( event ) {   
-    event.target.style.opacity = 0.5;
-
-    setTimeout(function() {
-      event.target.style.opacity = 0;
-    }, 1000);
-  }, false);
+$("#IMG01") .mouseover(function() {
+    this.src= "http://anderscr.cs321.simmons.edu/images/CloserLook1.png"
+}).mouseout(function () {
+    this.src= "http://anderscr.cs321.simmons.edu/images/IMG01.JPG"
+});
