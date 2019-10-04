67b06ebc6b2e77d2a40ab1f29bd2731ae2f4ee07
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-04 11:12:43 -0400

    Update contact.php

--- www/contact_me/contact.php
+++ www/contact_me/contact.php
@@ -1,1 +1,29 @@
-hi
+<!doctype html>
+
+<html lang = "en">
+<head>
+     
+<!-- Author:  Caitlin Anderson
+     Purpose: CS321 Assignment 2 --> 
+
+  <link rel="stylesheet" href="/css/style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
+  <script src="/javascript/myscripts.js"></script>
+</head>
+  <nav class = "header">         
+    <a><h2>Name</h2></a>
+    <ul class = "hov">
+      <li class = crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
+      <li class = crumb><a href = "gallery 2">Gallery 2</a></li>
+      <li class = crumb><a href = "shop">Shop</a></li>
+      <li class = crumb><a href = "bio">Bio</a></li>
+    </ul>
+<!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
+  </nav>   <!-- I want the nav as a seperate column to the left -->
+
+  <footer>
+    <a>copyright information</a>
+    <a href = "contact" class = "contact">Contact Me</a>
+  </footer>
+<!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
+       </html>
