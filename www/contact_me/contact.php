1f8e93eeaf8fc012e99fa6f739702724ef292973
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-04 12:58:02 -0400

    Update contact.php

--- www/contact_me/contact.php
+++ www/contact_me/contact.php
@@ -73,7 +73,7 @@
 <p><button class = "checkout" >Checkout</button></p>
   <footer>
     <a>copyright information</a>
-    <a href = "contact" class = "contact">Contact Me</a>
+    <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class = "contact">Contact Me</a>
   </footer>
 <!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
        </html>
