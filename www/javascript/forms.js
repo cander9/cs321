c56439b335c526d40526d1feebdbe12988dc69d6
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-09 02:58:01 -0400

    Update forms.js

--- www/javascript/forms.js
+++ www/javascript/forms.js
@@ -3,7 +3,7 @@ function checkName1(){
     var errorname1 = document.getElementById( "errorname1" );
     if( name1 === "" ){
         console.log( "Empty" );
-        errorname1.innerHTML = "your name is required to complete this form";
+        errorname1.innerHTML = "your name is required to complete this form"
         errorname1.classList.add( "showerror" );
         console.log( errorname1 );
     } else {
@@ -20,12 +20,34 @@ function checkName2(){
     if( name2 === "" ){
         console.log( "Empty" );
         errorname2.innerHTML = "your name is required to complete this form";
-        errorname2.classList.add( "showerror" );
+        errorname2.classList.add( "showerror");
         console.log( errorname2 );
     } else {
         console.log( "NOT Empty" );
         errorname2.innerHTML = "";
         errorname2.classList.remove( "showerror" );
         console.log( errorname2 );
+    }
+}
+
+function checkPhone(){
+    var phone = document.getElementById( "phone" ).value.trim() ;
+    var errorphone = document.getElementById( "errorphone" );
+    if( phone === "" ){
+        console.log( "Empty" );
+        errorphone.innerHTML = "your phone number is required to complete this form";
+        errorphone.classList.add( "showerror" );
+        console.log( errorphone );
+    } else if(phone !== "000-000-0000"){
+        console.log( "Wrong format" );
+        errorphone.innerHTML = "your phone number must be in ###-###-#### format";
+        errorphone.classList.add( "showerror" );
+        console.log( errorphone );
+    }
+    else {
+        console.log( "NOT Empty" );
+        errorphone.innerHTML = "";
+        errorphone.classList.remove( "showerror" );
+        console.log( errorphone );
     }
 }
