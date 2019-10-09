07116a0fbf5d28e9ad8fd9707ea961cbeaba47e4
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-09 19:43:52 -0400

    Update gallery1B.php

--- www/closer_look/gallery1B.php
+++ www/closer_look/gallery1B.php
@@ -1,6 +1,6 @@
 <!doctype html>
 
-<html lang = "en">
+<html lang="en">
 <head>
      
 <!-- Author:  Caitlin Anderson
@@ -10,25 +10,28 @@
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
 </head>
 <body>     
-  <nav class = "header">         
+<header>  
+   <nav class="header">         
     <a><h2>Name</h2></a>
     <ul>
-      <li class = crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
-      <li class = crumb><a href = "gallery 2">Gallery 2</a></li>
-      <li class = crumb><a href = "shop">Shop</a></li>
-      <li class = crumb><a href = "bio">Bio</a></li>
+      <li class=crumb><a href="http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
+      <li class=crumb><a href="gallery 2">Gallery 2</a></li>
+      <li class=crumb><a href="shop">Shop</a></li>
+      <li class=crumb><a href="bio">Bio</a></li>
     </ul>
 <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
   </nav>   <!-- I want the nav as a seperate column to the left -->
-  <header> <h1>Gallery 1</h1>
+  <div>
+  <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </header>   <!-- will be a description of the gallery in final -->
   <section>
-    <img   src="http://anderscr.cs321.simmons.edu/images/IMG1.JPG" width = 600/>
+    <img   src="http://anderscr.cs321.simmons.edu/images/IMG1.JPG" width=600/>
   </section>   <!-- will be a picture of the art in final, I want it centered and big-->
+  </div>
   <footer>
     <a>copyright information</a>
-    <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class = "contact">Contact Me</a>
+    <a href="http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
   </footer>
-  </body>   
+</body>     
 </html><!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
