43e20382b688e61be9c3893a248ee5a8d9f648e1
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-09 13:25:55 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -10,6 +10,7 @@
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
   <script src="/javascript/myscripts.js"></script>
 </head>
+<body>     
   <nav class = "header">         
     <a><h2>Name</h2></a>
     <ul class = "hov">
@@ -42,4 +43,5 @@
     <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class = "contact">Contact Me</a>
   </footer>
 <!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
-  </html>
+     </body>
+     </html>
