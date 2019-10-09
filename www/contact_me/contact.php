4a8205137eac8ef43efd17ae83f9bed81c270cc6
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-09 15:08:29 -0400

    Update contact.php

--- www/contact_me/contact.php
+++ www/contact_me/contact.php
@@ -22,66 +22,66 @@
     <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
 </nav>   <!-- I want the nav as a seperate column to the left -->
 <form action="#" method="POST">
-<div>
-    <label for="name1">Name : </label>
-    <div id="errorname1" class="error"></div>
-    <input  class = "regInput" type="text" id="name1" name="name1" minlength="2">
-</div>
-<div>
-    <label for = "phone">Phone : </label>
-    <div id="errorphone" class="error"></div>
-    <input  class = "regInput" type="tel" id = "phone" name="phone">
-</div>
-<div>
-    <label for = "email">Email : </label>
-    <div id="erroremail" class="error"></div>
-    <input  class = "regInput" type="email" id = "email" name="email">
-</div>
-<fieldset class = "f1">
-    <legend>Shipping Address</legend>
     <div>
-        <label for="name2">Name: </label>
-        <div id="errorname2" class="error"></div>
-        <input class = "shipInput" type="text" id="name2" name="name2" minlength="2">
+        <label for="name1">Name : </label>
+        <div id="errorname1" class="error"></div>
+        <input  class = "regInput" type="text" id="name1" name="name1" minlength="2">
     </div>
     <div>
-        <label>Address Line 1
-            <input  class = "shipInput" type="text" name="address_line1">
-        </label>
+        <label for = "phone">Phone : </label>
+        <div id="errorphone" class="error"></div>
+        <input  class = "regInput" type="tel" id = "phone" name="phone">
     </div>
     <div>
-        <label>Address Line 2
-            <input class = "shipInput" type="text" name="address_line2">
-        </label>
+        <label for = "email">Email : </label>
+        <div id="erroremail" class="error"></div>
+        <input  class = "regInput" type="email" id = "email" name="email">
     </div>
-</fieldset>
-<fieldset class = "f2">
-    <legend>Description</legend>
-    <div>
-        <label>Size :</label>
-        <p> </p>
-        <input type="radio" name="size" value="small"> Small &#40;5X7 in. or comparable&#41;<br>
-        <input type="radio" name="size" value="medium"> Medium &#40;16X20 in. or comparable&#41;<br>
-        <input type="radio" name="size" value="large"> Larger than 16X20 &#40;please specify in your description&#41;<br>
-    </div>
-    <div>
-        <p></p>
-        <label>These are my checkboxes:</label>
-        <p></p>
-        <input type="checkbox" name="hi1" value="hi1"> here<br>
-        <input type="checkbox" name="hi2" value="hi2"> they<br>
-        <input type="checkbox" name="hi3" value="hi3"> are<br>
-    </div>
-    <p></p>
-    <div>
-        <label>Describe :
+    <fieldset class = "f1">
+        <legend>Shipping Address</legend>
+        <div>
+            <label for="name2">Name: </label>
+            <div id="errorname2" class="error"></div>
+            <input class = "shipInput" type="text" id="name2" name="name2" minlength="2">
+        </div>
+        <div>
+            <label>Address Line 1
+                <input  class = "shipInput" type="text" name="address_line1">
+            </label>
+        </div>
+        <div>
+            <label>Address Line 2
+                <input class = "shipInput" type="text" name="address_line2">
+            </label>
+        </div>
+    </fieldset>
+    <fieldset class = "f2">
+        <legend>Description</legend>
+        <div>
+            <label>Size :</label>
+            <p> </p>
+            <input type="radio" name="size" value="small"> Small &#40;5X7 in. or comparable&#41;<br>
+            <input type="radio" name="size" value="medium"> Medium &#40;16X20 in. or comparable&#41;<br>
+            <input type="radio" name="size" value="large"> Larger than 16X20 &#40;please specify in your description&#41;<br>
+        </div>
+        <div>
             <p></p>
-            <textarea class= "describe" name="describe"></textarea>
-        </label>
-    </div>
-    <p></p>
-</fieldset>
-<p><input type="button" class="submit" onclick="checkName1(),checkName2(),checkPhone(),checkEmail();" value="Submit"/></p>
+            <label>These are my checkboxes:</label>
+            <p></p>
+            <input type="checkbox" name="hi1" value="hi1"> here<br>
+            <input type="checkbox" name="hi2" value="hi2"> they<br>
+            <input type="checkbox" name="hi3" value="hi3"> are<br>
+        </div>
+        <p></p>
+        <div>
+            <label>Describe :
+                <p></p>
+                <textarea class= "describe" name="describe"></textarea>
+            </label>
+        </div>
+        <p></p>
+    </fieldset>
+    <p><input type="button" class="submit" onclick="checkAll();" value="Submit"/></p>
 </form>
 <footer>
     <a>copyright information</a>
