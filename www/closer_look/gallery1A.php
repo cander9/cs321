2b739c259f61996a3d02e3eacb951af9a7bba9f8
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-09 19:49:17 -0400

    Update gallery1A.php

--- www/closer_look/gallery1A.php
+++ www/closer_look/gallery1A.php
@@ -21,14 +21,12 @@
     </ul>
 <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
   </nav>   <!-- I want the nav as a seperate column to the left -->
-  <div>
   <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </header>   <!-- will be a description of the gallery in final -->
-  <section>
+  <div>
     <img   src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" width=600/>
-  </section>   <!-- will be a picture of the art in final, I want it centered and big-->
-  </div>
+  </div>   <!-- will be a picture of the art in final, I want it centered and big-->
   <footer>
     <a>copyright information</a>
     <a href="http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
