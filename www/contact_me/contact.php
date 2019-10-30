8c77a15f29829ecd10da23d8034b706f48dec1e2
cander9 <49759197+cander9@users.noreply.github.com>
2019-10-30 13:03:30 -0400

    Update contact.php

--- www/contact_me/contact.php
+++ www/contact_me/contact.php
@@ -154,9 +154,9 @@ function test_input($data) {
             <label>These are my checkboxes:</label>
             <p></p>
             <div id="errorcheck" class="error"><?php echo "<p class='note'>". $checkERR;?></div>
-            <input id="check1" type="checkbox" name="check[]" <?php if(isset($_POST['submit']) &&$_POST['check'][0]) echo "checked" ?> value="here"> here<br>
+            <input id="check1" type="checkbox" name="check[]" <?php if(isset($_POST['submit']) && isset($_POST['check'][0])) echo "checked" ?> value="here"> here<br>
             <input id="check2" type="checkbox" name="check[]" <?php if(isset($_POST['submit']) && isset($_POST['check'][1])) echo "checked" ?> value="they"> they<br>
-            <input id="check3" type="checkbox" name="check[]" <?php if(isset($_POST['submit']) &&$_POST['check'][2]) echo "checked" ?> value="are"> are<br>
+            <input id="check3" type="checkbox" name="check[]" <?php if(isset($_POST['submit']) && isset($_POST['check'][2])) echo "checked" ?> value="are"> are<br>
         </div>
         <!--<p></p>
         <div>
