fa2f9fc1dab73a11df5874fed0e47e30174d0b34
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-06 18:44:32 -0500

    Update gallery1C.php

--- www/closer_look/gallery1C.php
+++ www/closer_look/gallery1C.php
@@ -8,6 +8,7 @@
 
   <link rel="stylesheet" href="/css/closer_style.css" />
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>     
 <header>  
