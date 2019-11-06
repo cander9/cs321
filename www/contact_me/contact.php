0ef5a425c82b9bfc71c66f0584712169f32fc18d
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-06 18:45:13 -0500

    Update contact.php

--- www/contact_me/contact.php
+++ www/contact_me/contact.php
@@ -97,6 +97,7 @@ function test_input($data) {
     <link rel="stylesheet" href="/css/contactStyle.css" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
     <script src="/javascript/forms.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <nav class="header">
     <a><h2>Name</h2></a>
     <ul class="hov">
