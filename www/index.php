c48ea06ffcd094205ae4b66fe7ce9682b8cdec86
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-13 18:55:43 -0500

    Update index.php

--- www/index.php
+++ www/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="/css/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
     <script src="/javascript/myscripts.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <nav class="header">
         <a><h2>Name</h2></a>
         <ul class="hov">
