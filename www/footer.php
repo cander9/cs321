2df01b45b1eb72603b1166742404dc9f61bbb8f9
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-13 18:39:14 -0500

    Create footer.php

---
+++ www/footer.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<body>
+<footer>
+    <a>copyright information</a>
+    <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
+</footer>
+</body>
+</html>
