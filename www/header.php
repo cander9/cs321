3ef1f885621600d746c93498e017128d7ed578f8
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-13 18:38:49 -0500

    Create header.php

---
+++ www/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang = "en">
+<head>
+
+    <!-- Author:  Caitlin Anderson
+         Purpose: CS321 Assignment-->
+
+    <link rel="stylesheet" href="/css/contactStyle.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
+    <script src="/javascript/forms.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <nav class="header">
+        <a><h2>Name</h2></a>
+        <ul class="hov">
+            <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
+            <li class=crumb><a href = "gallery 2">Gallery 2</a></li>
+            <li class=crumb><a href = "shop">Shop</a></li>
+            <li class=crumb><a href = "bio">Bio</a></li>
+        </ul>
+        <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
+    </nav>
+</head>
+</html>
