d3685df10c552a1464138b62a40bd658d845dcb3
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 16:16:15 -0500

    Create option3.php

---
+++ www/option3.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="en">
+#include file="header.php"
+<body>
+<div>
+    <ul id="test">
+        <li class="container">
+            <img src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" alt="Piece1" width=300>
+            <a class="closerLook" id="closerLook1" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1A.php">Closer Look</a>
+        </li>
+        <li class= "container" >
+            <img src="http://anderscr.cs321.simmons.edu/images/IMG1.JPG" alt="Piece2" width=300 class="rotate">
+            <a class="closerLook" id="closerLook2" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1B.php">Closer Look</a>
+        </li>
+        <li class="container">
+            <img src="http://anderscr.cs321.simmons.edu/images/IMG2.JPG" alt="Piece3" width=300 class="rotate">
+            <a class="closerLook" id="closerLook3" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1C.php">Closer Look</a>
+        </li>
+    </ul>
+</div> <!-- However Many I need --><!--Code Source:  https://www.w3schools.com/howto/howto_css_button_on_image.asp    eventually I want to be able to click on the img rather than have a separate button-->
+<footer>
+    <a>copyright information</a>
+    <a href="http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
+</footer>
+<!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
+</body>
+</html>
