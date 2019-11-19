4bdf43453293f4413d68e322e31468e4c30f59d6
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 17:05:39 -0500

    Update gallery1C.php

--- www/closer_look/gallery1C.php
+++ www/closer_look/gallery1C.php
@@ -27,10 +27,10 @@
         <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
     </nav>
 </head>
-  <div>
+  <section>
     <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
+  </section>
   <div>
     <img   src="http://anderscr.cs321.simmons.edu/images/IMG2.JPG" width=600/>
   </div>   <!-- will be a picture of the art in final, I want it centered and big-->
