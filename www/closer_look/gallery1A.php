067de52e651c25259c5a473c70e2e6f471e21fb9
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 16:37:07 -0500

    Update gallery1A.php

--- www/closer_look/gallery1A.php
+++ www/closer_look/gallery1A.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <html lang="en">
-<?php include('header.php'); ?>
+<?php include('../header.php'); ?>
   <div>
     <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -9,6 +9,6 @@
   <div>
     <img   src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" width=600/>
   </div>   <!-- will be a picture of the art in final, I want it centered and big-->
-<?php include('footer.php'); ?>
+<?php include('../footer.php'); ?>
 </body>     
 </html><!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
