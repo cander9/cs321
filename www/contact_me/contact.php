9ccd611e844e968df325a6dfe220ada2cbba14ae
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 16:32:00 -0500

    Update contact.php

--- www/contact_me/contact.php
+++ www/contact_me/contact.php
@@ -89,26 +89,7 @@ function test_input($data) {
 ?>
 <!doctype html>
 <html lang = "en">
-<head>
-
-    <!-- Author:  Caitlin Anderson
-         Purpose: CS321 Assignment 2 -->
-
-    <link rel="stylesheet" href="/css/contactStyle.css" />
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
-    <script src="/javascript/forms.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<nav class="header">
-    <a><h2>Name</h2></a>
-    <ul class="hov">
-        <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
-        <li class=crumb><a href = "gallery 2">Gallery 2</a></li>
-        <li class=crumb><a href = "shop">Shop</a></li>
-        <li class=crumb><a href = "bio">Bio</a></li>
-    </ul>
-    <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
-</nav>   <!-- I want the nav as a separate column to the left -->
-</head>
+<?php include('header.php'); ?>
 <body>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST"> <!-- https://www.w3schools.com/php/php_form_validation.asp -->
     <div>
@@ -174,10 +155,6 @@ function test_input($data) {
     </fieldset>
     <p><input type="submit" class="submit" value="Submit"/></p><!--onclick="checkAll();"-->
 </form>
-<footer>
-    <a>copyright information</a>
-    <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
-</footer>
-<!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
+<?php include('footer.php'); ?>
 </body>
 </html>
