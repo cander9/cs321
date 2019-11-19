478247e023e9ecf2723cccd5530eb0564cf15db0
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 16:22:45 -0500

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,35 +1,7 @@
 <!doctype html>
 
 <html lang="en">
-<head>
-
-    <!-- Author:  Caitlin Anderson
-         Purpose: CS321 Assignment 2 -->
-
-    <link rel="stylesheet" href="/css/style.css" />
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
-    <script src="/javascript/myscripts.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152530601-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-152530601-1');
-    </script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <nav class="header">
-        <a><h2>Name</h2></a>
-        <ul class="hov">
-            <li class=crumb><a href="http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
-            <li class=crumb><a href="gallery 2">Gallery 2</a></li>
-            <li class=crumb><a href="shop">Shop</a></li>
-            <li class=crumb><a href="bio">Bio</a></li>
-        </ul>
-        <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
-    </nav>   <!-- I want the nav as a seperate column to the left -->
-</head>
+<?php include('header.php'); ?>
 <body>
     <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
