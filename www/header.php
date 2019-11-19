c27d91cd7f2f1327177642ec6ca6f0facfdcf7d1
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 17:12:37 -0500

    Update header.php

--- www/header.php
+++ www/header.php
@@ -15,7 +15,6 @@
         <ul class="hov">
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/option3.php">Gallery 2</a></li>
-            <li class=crumb><a href = "shop">Shop</a></li>
             <li class=crumb><a href = "bio">Bio</a></li>
         </ul>
         <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
