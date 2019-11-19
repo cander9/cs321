c8858eb23dfa3573174b908cc6de654311fbb8dd
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-19 16:33:50 -0500

    Update gallery1A.php

--- www/closer_look/gallery1A.php
+++ www/closer_look/gallery1A.php
@@ -1,36 +1,14 @@
 <!doctype html>
 
 <html lang="en">
-<head>
-     
-<!-- Author:  Caitlin Anderson
-     Purpose: CS321 Assignment 2 --> 
-
-  <link rel="stylesheet" href="/css/closer_style.css" />
-  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>     
-<header>  
-   <nav class="header">         
-    <a><h2>Name</h2></a>
-    <ul>
-      <li class=crumb><a href="http://anderscr.cs321.simmons.edu/">Gallery 1</a></li>
-      <li class=crumb><a href="gallery 2">Gallery 2</a></li>
-      <li class=crumb><a href="shop">Shop</a></li>
-      <li class=crumb><a href="bio">Bio</a></li>
-    </ul>
-<!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
-  </nav>   <!-- I want the nav as a seperate column to the left -->
-  <h1>Gallery 1</h1>
+<?php include('header.php'); ?>
+  <div>
+    <h1>Gallery 1</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </header>   <!-- will be a description of the gallery in final -->
+  </div>
   <div>
     <img   src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" width=600/>
   </div>   <!-- will be a picture of the art in final, I want it centered and big-->
-  <footer>
-    <a>copyright information</a>
-    <a href="http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
-  </footer>
+<?php include('footer.php'); ?>
 </body>     
 </html><!-- I got the section names, nav, aside, footer, etc from https://www.w3schools.com/html/html_layout.asp -->
