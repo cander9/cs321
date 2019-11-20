10ccfd2c4ce238141461085e9868513b488d88dd
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-20 16:27:01 -0500

    Update footer.php

--- www/footer.php
+++ www/footer.php
@@ -1,9 +1,4 @@
-<!doctype html>
-<html lang="en">
-<body>
 <footer>
     <a>copyright information</a>
     <a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php" class="contact">Contact Me</a>
 </footer>
-</body>
-</html>
