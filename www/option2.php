bddf5cbbdc2f2cd30186a86422625d8b8e8b872f
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-20 13:54:46 -0500

    Update option2.php

--- www/option2.php
+++ www/option2.php
@@ -1,8 +1,41 @@
+<?php
+function feed($feedURL){
+    $i = 0; // initiate counter to limit the amount of articles to return
+    $url = $feedURL; // url to parse
+    $rss = simplexml_load_file($url); // the XML parser
+    // RSS items loop
+    foreach($rss->channel->item as $item) {  //loop through each item
+        $link = $item->link;  //extract the link
+        $title = $item->title;  //extract the title
+        $date = $item->pubDate;  //extract the date
+        $description = strip_tags($item->description);  //extract description and strip HTML
+        if (strlen($description) > 200) {
+            // truncate string if greater than 200 characters
+            $stringCut = substr($description, 0, 200);
+            // make sure it ends in a complete word and add ... at the end
+            $description = substr($stringCut, 0, strrpos($stringCut, ' ')).'...';
+        }
+        if ($i < 4) { // parse only 4 items
+            echo '
+                        <a class="list-group-item" href="'.$link.'" target="_blank">
+                            <h5 class="list-group-item-heading">'.$title.'<br><small>'.$date.'</small></h5>
+                            <p class="list-group-item-text">'.$description.'</p>
+                        </a>
+                        ';
+        }
+        $i++;
+    }
+}
+?>
+
 <!doctype html>
 
 <html lang="en">
 <?php include('header.php'); ?>
 <body>
+<div>
+    <?php feed("https://ptwc.weather.gov/feeds/ptwc_rss_hawaii.xml") ?>
+</div>
 <div>
     <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B5EyYGKH62w/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="12" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:500px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:16px;">
             <a href="https://www.instagram.com/p/B5EyYGKH62w/?utm_source=ig_embed&amp;utm_campaign=loading" style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;" target="_blank">
