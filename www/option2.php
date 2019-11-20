19d8fb87053a5a4cccbedd51f7d48e6daf22c276
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-20 15:22:26 -0500

    Update option2.php

--- www/option2.php
+++ www/option2.php
@@ -1,29 +1,17 @@
 <?php
 function feed($feedURL){
-    $i = 0; // initiate counter to limit the amount of articles to return
-    $url = $feedURL; // url to parse
-    $rss = simplexml_load_file($url); // the XML parser
+    $i = 0;
+    $url = $feedURL;
+    $rss = simplexml_load_file($url);
     // RSS items loop
     foreach($rss->channel->item as $item) {  //loop through each item
         $link = $item->link;  //extract the link
         $title = $item->title;  //extract the title
         $date = $item->pubDate;  //extract the date
         $description = strip_tags($item->description);  //extract description and strip HTML
-        if (strlen($description) > 200) {
-            // truncate string if greater than 200 characters
-            $stringCut = substr($description, 0, 200);
-            // make sure it ends in a complete word and add ... at the end
-            $description = substr($stringCut, 0, strrpos($stringCut, ' ')).'...';
-        }
-        if ($i < 4) { // parse only 4 items
-            echo '
-                        <a class="list-group-item" href="'.$link.'" target="_blank">
-                            <h5 class="list-group-item-heading">'.$title.'<br><small>'.$date.'</small></h5>
-                            <p class="list-group-item-text">'.$description.'</p>
-                        </a>
-                        ';
-        }
-        $i++;
+    }
+    foreach($rss->channel->image as $item){
+        $IMAGEurl = $item->url;
     }
 }
 ?>
