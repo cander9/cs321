75c160bde8069c1f2affba604131431d92167201
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-20 16:04:53 -0500

    Update option2.php

--- www/option2.php
+++ www/option2.php
@@ -8,25 +8,12 @@ function feed($feedURL){
         $link = $item->link;  //extract the link
         $title = $item->title;  //extract the title
         $date = $item->pubDate;  //extract the date
-        $description = strip_tags($item->description);  //extract description and strip HTML
+        $description = $item->description;  //extract description and strip HTML
         echo '
 <a class="list-group-item" href="'.$link.'" target="_blank">
     <h5 class="list-group-item-heading">'.$title.'<br><small>'.$date.'</small></h5>
     <p class="list-group-item-text">'.$description.'</p>
 </a>
-        ';
-    }
-
-    foreach($rss->channel->image as $item){
-        $IMAGEurl = $item->url;
-        $IMAGEalt = $item->title;
-        $IMAGElink = $item->link;
-        echo '
-<img>
-    <url>'.$IMAGEurl.'</url>
-    <title>'.$IMAGEalt.'</title>
-    <link>'.$IMAGElink.'</link>
-</img>
         ';
     }
 }
@@ -37,7 +24,10 @@ function feed($feedURL){
 <html lang="en">
 <?php include('header.php'); ?>
 <body>
-<?php feed("https://ptwc.weather.gov/feeds/ptwc_rss_hawaii.xml") ?>
+<div>
+    <?php feed("https://ptwc.weather.gov/feeds/ptwc_rss_hawaii.xml") ?>
+    <img src="https://ptwc.weather.gov/ptwc/images/npacific_map.jpg" alt=map">
+</div> <!-- However Many I need --><!--Code Source:  https://www.w3schools.com/howto/howto_css_button_on_image.asp    eventually I want to be able to click on the img rather than have a separate button-->
 <?php include('footer.php'); ?>
 </body>
 </html>
