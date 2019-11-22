f1177cd5ebef7ab946256fb44197c866eeb0d85a
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-22 14:12:15 -0500

    Update closer1.php

--- www/closer_look/closer1.php
+++ www/closer_look/closer1.php
@@ -4,7 +4,7 @@
 <body>
     <a href="http://anderscr.cs321.simmons.edu/" class="x">x</a>
     <ul class="closeUP">
-        <li><img src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" alt="portrait 1"/></li>
+        <li><img src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" alt="portrait 1" width=1000/></li>
         <li><a href="anderscr.cs321.simmons.edu/closer_look/closer2.php" class="next">&#62;</a></li>
     </ul>
 </body>
