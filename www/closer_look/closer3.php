fb62e9395ddafd6eb239a6267b4c4e4bc276d31f
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-22 14:08:48 -0500

    Create closer3.php

---
+++ www/closer_look/closer3.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<html lang="en">
+<body>
+    <a href="http://anderscr.cs321.simmons.edu/" class="x">x</a>
+    <ul class="closeUP">
+        <li><a href="anderscr.cs321.simmons.edu/closer_look/closer2.php" class="last">&#60;</a></li>
+        <li><img src="http://anderscr.cs321.simmons.edu/images/IMG2.JPG" alt="portrait 3"/></li>
+    </ul>
+</body>
+</html>
