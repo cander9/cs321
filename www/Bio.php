b44b4c0765ff56da3de37ded1a9cb76c79f07044
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-22 09:41:00 -0500

    Update Bio.php

--- www/Bio.php
+++ www/Bio.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <?php include('header.php'); ?>
 <body>
-  <img src="http://anderscr.cs321.simmons.edu/images/IMG_2738.jpeg" alt="cat"/>
+  <img src="http://anderscr.cs321.simmons.edu/images/IMG_2738.jpeg" alt="cat" width=300 class="cat" />
 <?php include('footer.php'); ?>
 </body>
 </html>
