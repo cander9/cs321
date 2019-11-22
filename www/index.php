cc70e7dc3a7250176398655fa1ccf299d5d58c56
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-22 14:09:43 -0500

    Update index.php

--- www/index.php
+++ www/index.php
@@ -9,15 +9,15 @@
     <ul id="test">
         <li class="container">
             <img src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" alt="Piece1" width=300>
-            <a class="closerLook" id="closerLook1" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1A.php">Closer Look</a>
+            <a class="closerLook" id="closerLook1" href="http://anderscr.cs321.simmons.edu/closer_look/closer1.php">Closer Look</a>
         </li>
         <li class="container" >
             <img src="http://anderscr.cs321.simmons.edu/images/IMG1.JPG" alt="Piece2" width=300 class="rotate">
-            <a class="closerLook" id="closerLook2" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1B.php">Closer Look</a>
+            <a class="closerLook" id="closerLook2" href="http://anderscr.cs321.simmons.edu/closer_look/closer2.php">Closer Look</a>
         </li>
         <li class="container">
             <img src="http://anderscr.cs321.simmons.edu/images/IMG2.JPG" alt="Piece3" width=300 class="rotate">
-            <a class="closerLook" id="closerLook3" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1C.php">Closer Look</a>
+            <a class="closerLook" id="closerLook3" href="http://anderscr.cs321.simmons.edu/closer_look/closer3.php">Closer Look</a>
         </li>
     </ul>
 </div> <!-- However Many I need --><!--Code Source:  https://www.w3schools.com/howto/howto_css_button_on_image.asp    eventually I want to be able to click on the img rather than have a separate button-->
