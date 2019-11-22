930a7c29f2564376b45c28cb29d0be33f2879182
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-22 14:24:07 -0500

    Create closerHead.php

---
+++ www/closer_look/closerHead.php
@@ -0,0 +1,15 @@
+<head>
+
+    <!-- Author:  Caitlin Anderson
+         Purpose: CS321 Final-->
+
+    <link rel="stylesheet" href="/css/contactStyle.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-152530601-1');
+    </script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
