3509294dbf720a8eb9a54aed65bcca89e8722c93
cander9 <49759197+cander9@users.noreply.github.com>
2019-11-21 20:34:54 -0500

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,14 +4,14 @@
 <?php include('header.php'); ?>
 <body>
     <h1>Gallery 1</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p class="galleryDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 <div>
     <ul id="test">
         <li class="container">
             <img src="http://anderscr.cs321.simmons.edu/images/IMG01.JPG" alt="Piece1" width=300>
             <a class="closerLook" id="closerLook1" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1A.php">Closer Look</a>
         </li>
-        <li class= "container" >
+        <li class="container" >
             <img src="http://anderscr.cs321.simmons.edu/images/IMG1.JPG" alt="Piece2" width=300 class="rotate">
             <a class="closerLook" id="closerLook2" href="http://anderscr.cs321.simmons.edu/closer_look/gallery1B.php">Closer Look</a>
         </li>
