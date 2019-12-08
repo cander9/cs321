a4d44557ca2604f7e27d470c99e1da6c3e3f00ca
cander9 <49759197+cander9@users.noreply.github.com>
2019-12-08 15:56:45 -0500

    Update Bio.php

--- www/Bio.php
+++ www/Bio.php
@@ -4,7 +4,7 @@ include 'db_connection.php';
 $conn = OpenCon();
 $qry = mysqli_query($conn, "SELECT * FROM iFrameURLs");
 while ($row = $qry->fetch_assoc()) {
-    echo $row['acl_table']."<br>";
+    echo $row['query']."<br>";
 }
 CloseCon($conn);
 
