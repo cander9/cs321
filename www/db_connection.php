11ef40eefa2ecd2e2e8ad344bccbc12f16a4fcba
cander9 <49759197+cander9@users.noreply.github.com>
2019-12-08 15:33:41 -0500

    Update db_connection.php

--- www/db_connection.php
+++ www/db_connection.php
@@ -5,9 +5,13 @@ function OpenCon()
     $dbuser = "anderscr";
     $dbpass = "1834579";
     $db = "iFrameURLs";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
 
-    return $conn;
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+
 }
 
 function CloseCon($conn)
