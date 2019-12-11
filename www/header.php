aedb3eb30fcf40d5ed42fca5d0629d2192debdae
cander9 <49759197+cander9@users.noreply.github.com>
2019-12-11 14:44:02 -0500

    Update header.php

--- www/header.php
+++ www/header.php
@@ -8,8 +8,8 @@
     <script src="/javascript/forms.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <div class="header">
+        <a><h2>Caitlin Anderson</h2></a>
         <ul class="hov">
-            <a><h2>Caitlin Anderson</h2></a>
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery</a></li>            
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/Bio.php">About</a></li>
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php">Contact Me</a></li>
