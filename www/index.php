062a17933f61e90ab783fdeae4b4a65a1d5f0bfc
cander9 <49759197+cander9@users.noreply.github.com>
2019-12-11 14:48:10 -0500

    Update index.php

--- www/index.php
+++ www/index.php
@@ -2,7 +2,6 @@
 
 <html lang="en">
 <?php include('header.php'); ?>
-<body>
     <h1>Gallery</h1>
     <p class="galleryDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 <div>
