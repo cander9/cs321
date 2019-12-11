61ea8a3367a6ee1f1b4a6899d0ebeced10207a89
cander9 <49759197+cander9@users.noreply.github.com>
2019-12-11 14:39:54 -0500

    Update header.php

--- www/header.php
+++ www/header.php
@@ -2,12 +2,12 @@
 
     <!-- Author:  Caitlin Anderson
          Purpose: CS321 Assignment-->
-
+    <title>Landing Page</title>
     <link rel="stylesheet" href="/css/contactStyle.css" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i&display=swap" rel="stylesheet">
     <script src="/javascript/forms.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <nav class="header">
+    <div class="header">
         <a><h2>Caitlin Anderson</h2></a>
         <ul class="hov">
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/">Gallery</a></li>            
@@ -15,5 +15,5 @@
             <li class=crumb><a href = "http://anderscr.cs321.simmons.edu/contact_me/contact.php">Contact Me</a></li>
         </ul>
         <!-- source:  https://developer.mozilla.org/enUS/docs/Web/HTML/Element/nav -->
-    </nav>
+    </div>
 </head>
